14903f05e5b9ce7806beb7f17c26fee9ec0ffeff
Grace Kim <130432110+haeunkim48@users.noreply.github.com>
2023-11-01 23:14:00 +0900

    hangman

---
+++ hangman/hangman.py
@@ -0,0 +1,32 @@
+import random
+import string
+from words import words
+
+def get_valid_word(words):
+    word = random.choice(words)
+    while '-' in word or ' ' in word:
+        word = random.choice(words)
+    return word.upper()
+
+def hangman():
+    word = get_valid_word(words)
+    word_letters = set(word)
+    alphabet = set(string.ascii_uppercase)
+    used_letters = set()
+
+    while len(word_letters) > 0:
+        user_letter = input('Guess a letter: ').upper()
+        
+        if user_letter in alphabet - used_letters:
+            used_letters.add(user_letter)
+            if user_letter in word_letters:
+                word_letters.remove(user_letter)
+        elif user_letter in used_letters:
+            print('You have already used that character. Please try again.')
+        else:
+            print('Invalid character. Please try again.')
+
+    print(f'Congratulations! You guessed the word: {word}')
+
+if __name__ == "__main__":
+    hangman()
