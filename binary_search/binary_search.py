366a5a3034ccf8a7dd3208a2bd24715a4ed59a38
Grace Kim <130432110+haeunkim48@users.noreply.github.com>
2023-11-13 23:31:23 +0900

    add

---
+++ binary_search/binary_search.py
@@ -0,0 +1,47 @@
+# Implementation of binary search algorithm 
+
+# We will prove that binary search is faster than naive search 
+
+# naive search: scan entire list and ask if its equal to the target 
+# if yes, retun the index
+# if no, then return - 1
+
+def naive_search (l, target):
+    # example ' = [1,3,10,12]
+    for l in range (len(l)):
+        if l[i] == target: 
+            return i 
+    return -1
+
+# binary search uses divide and conquer 
+# We will leverage the fact that our list is SORTED 
+
+def binary_search (l, target, low = None, high = None):
+    if low is None: 
+        low = 0 
+    
+    if high is None: 
+        high = len(l) -1
+    
+    if high < low: 
+        return -1 
+    
+    # example l = [1, 3, 5, 10, 12] # should return 3 
+    midpoint = (len(l)) // 2 # 2
+
+    if l [midpoint] == target: 
+        return midpoint 
+    elif target <l[midpoint]:
+        return binary_search (l,target, low, midpoint-1)
+    else: 
+        # target > l[midpoint]
+        return binary_search(l, target, midpoint + 1, high)
+    
+
+    
+if __name__ == '__main__':
+    
+     l = [1, 3, 5, 10, 12]
+     target = 10 
+     print (naive_search(l, target))
+     print (binary_search(l, target))
